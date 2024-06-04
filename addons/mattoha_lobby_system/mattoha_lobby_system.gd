bec73dc983d1c0f3bde3c8287b04024353833c4f
Tareq J <zer0xtj.code@gmail.com>
2024-06-04 12:16:26 +0300

    fixing issues

--- addons/mattoha_lobby_system/mattoha_lobby_system.gd
+++ addons/mattoha_lobby_system/mattoha_lobby_system.gd
@@ -7,7 +7,6 @@ func _enter_tree():
 	add_custom_type("MattohaSpawner", "Node2D", preload ("res://addons/mattoha_lobby_system/core/nodes/MattohaSpawner.cs"), null)
 	add_custom_type("MattohaServer", "Node2D", preload ("res://addons/mattoha_lobby_system/core/nodes/MattohaServer.cs"), null)
 	add_custom_type("MattohaClient", "Node2D", preload ("res://addons/mattoha_lobby_system/core/nodes/MattohaClient.cs"), null)
-	add_custom_type("MattohaGameHolder", "Node2D", preload ("res://addons/mattoha_lobby_system/core/nodes/MattohaGameHolder.cs"), null)
 
 	add_autoload_singleton("MattohaSystem", "res://addons/mattoha_lobby_system/core/autoload/MattohaSystem.tscn")
 
@@ -18,4 +17,3 @@ func _exit_tree():
 	remove_custom_type("MattohaSpawner")
 	remove_custom_type("MattohaServer")
 	remove_custom_type("MattohaClient")
-	remove_custom_type("MattohaGameHolder")
